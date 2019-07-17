46d035628d0eb5388e0598d65d2985554d49be05
Keith Cheveralls <keith.chev@gmail.com>
2019-07-16 17:11:51 -0700

    add columnDefs for facs results

--- src/definitions.js
+++ src/definitions.js
@@ -4,9 +4,11 @@ export const columnDefs = [
     {
         Header: 'Plate ID',
         accessor: 'plate_design_id',
+        width: 50,
     },{
         Header: 'Well ID',
         accessor: 'well_id',
+        width: 50,
     },{
         Header: 'EP date',
         id: 'electroporation_date',
@@ -18,15 +20,23 @@ export const columnDefs = [
     },{
         Header: 'Gene name',
         accessor: 'target_name',
+        width: 100,
     },{
         Header: 'Gene family',
         accessor: 'target_family',
+        width: 100,
     },{
         Header: 'Terminus',
         accessor: 'target_terminus',
+        width: 50,
     },{
         Header: 'ENST ID',
         accessor: 'transcript_id',
+        width: 150,
+    },{
+        Header: 'TPM (HEK293)',
+        accessor: 'hek_tpm',
+        width: 70,
     },{
         Header: 'Protospacer name',
         accessor: 'protospacer_name',
@@ -45,10 +55,43 @@ export const columnDefs = [
     },{
         Header: 'Template sequence',
         accessor: 'template_sequence',
+    },{
+        id: 'facs_area',
+        Header: 'FACS area',
+        accessor: row => {
+            if (row.facs) return row.facs.area;
+            return undefined;
+        }
+    },{
+        id: 'facs_rel_median_log',
+        Header: 'FACS intensity (median)',
+        accessor: row => {
+            if (row.facs) return row.facs.rel_median_log;
+            return undefined;
+        }
+    },{
+        id: 'facs_rel_percentile99_log',
+        Header: 'FACS intensity (max)',
+        accessor: row => {
+            if (row.facs) return row.facs.rel_percentile99_log;
+            return undefined;
+        }
+    },{
+        id: 'facs_raw_std',
+        Header: 'FACS width',
+        accessor: row => {
+            if (row.facs) return row.facs.raw_std;
+            return undefined;
+        }
+    },{
+        id: 'facs_plot',
+        Header: 'FACS plot',
+        accessor: row => null,
     },
 ];
 
 // copy id from accessor
+// *** WARNING: this assumes that columnDefs without an id have a string-valued accessor ***
 columnDefs.forEach(def => def.id = def.id ? def.id : def.accessor);
 
 // default selected columns
@@ -59,23 +102,39 @@ export const defaultSelectedColumns = [
 export const columnGroups = [
     {
         name: 'Metadata',
-        ids: ['plate_design_id', 'well_id', 'electroporation_date'],
+        ids: [
+            'plate_design_id', 
+            'well_id', 
+            'electroporation_date',
+        ],
     },{
         name: 'Target',
-        ids: ['target_name', 'target_family', 'target_terminus', 'transcript_id'],
+        ids: [
+            'target_name', 
+            'target_family', 
+            'target_terminus', 
+            'transcript_id', 
+            'hek_tpm'],
     },{
         name: 'Crispr design',
         ids: [
-            'protospacer_name', 'protospacer_notes', 'protospacer_sequence',
-            'template_name', 'template_notes', 'template_sequence',
+            'protospacer_name', 
+            'protospacer_notes', 
+            'protospacer_sequence',
+            'template_name', 
+            'template_notes', 
+            'template_sequence',
             ''
         ],
     },{
-        name: 'Expression',
-        ids: [],
-    },{
         name: 'FACS',
-        ids: [],
+        ids: [
+            'facs_area',
+            'facs_rel_median_log',
+            'facs_rel_percentile99_log',
+            'facs_raw_std',
+            'facs_plot',
+        ],
     },{
         name: 'Sequencing',
         ids: [],

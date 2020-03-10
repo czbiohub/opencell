5bd6f56411c9388160958ce079e3eee94d2f6e8c
Keith Cheveralls <keith.chev@gmail.com>
2020-03-10 13:31:05 -0700

    add FOV metadata to FOVCurator
    Former-commit-id: f81f05874adfea7341975d25d034f343521e187f

--- src/microscopy/FOVCurator.jsx
+++ src/microscopy/FOVCurator.jsx
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 import React, { Component } from 'react';
-import ReactTable from 'react-table';
 
+import classNames from 'classnames';
 import { Button, Radio, RadioGroup, MenuItem } from "@blueprintjs/core";
 import { Select } from "@blueprintjs/select";
 
@@ -15,15 +15,17 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 
 import settings from '../common/settings.js';
 import * as utils from '../common/utils.js';
+import { SectionHeader } from '../profile/common.jsx';
 
 import '../common/common.css';
 import '../profile/Profile.css';
 
 
-function thumbnail (fov, changeFov) {
+function thumbnail (fov, fovId, changeFov) {
+    const className = classNames('thumbnail', {'thumbnail-active': fov.id === fovId});
     return (
-        <div className="pa1" key={fov.id} onClick={() => changeFov(fov.id)}>
-            <img src={`data:image/jpg;base64,${fov.thumbnails.data}`}/>
+        <div className='pa1' key={fov.id} onClick={() => changeFov(fov.id)}>
+            <img className={className} src={`data:image/jpg;base64,${fov.thumbnails.data}`}/>
         </div>
     );
 }
@@ -36,8 +38,8 @@ export default class FOVCurator extends Component {
         super(props);
 
         this.state = {
-            loaded: false,
             fovId: null,
+            loaded: false,
         };
 
         this.changeFov = this.changeFov.bind(this);
@@ -48,18 +50,15 @@ export default class FOVCurator extends Component {
         this.setState({fovId});
     }
 
-
     componentDidMount () {
         this.fetchData();
     }
-
 
     componentDidUpdate(prevProps) {
         if (this.props.cellLineId!==prevProps.cellLineId) {
             this.fetchData();
         }
     }
-
 
     fetchData () {
         if (!this.props.cellLineId) return;
@@ -75,19 +74,40 @@ export default class FOVCurator extends Component {
 
     render () {
 
+        const fov = this.data?.fovs.filter(fov => fov.id === this.state.fovId)[0];
+
         return (
             <div className="">
 
-                <div className="w-33"></div>
+                <div className="flex">
 
-                {/* FOV display */}
-                <div className="w-66 tc">
-                    <img width='50%' src={`${settings.apiUrl}/fovs/rgb/proj/${this.state.fovId}`}/>
+                    <div className="w-20 pr3 flex" style={{flexDirection: 'column'}}>
+                        <SectionHeader title='FOV metadata'/>
+                        {FOVMetadataItem('FOV ID', fov?.id || 'NA', '')}
+                        {FOVMetadataItem('Laser power', fov?.laser_power_488.toFixed(1) || 'NA', '%')}
+                        {FOVMetadataItem('Exposure time', fov?.exposure_time_488.toFixed() || 'NA', 'ms')}
+                        {FOVMetadataItem('Max intensity', fov?.max_intensity_488 || 'NA', '')}
+                        {FOVMetadataItem('Score', fov?.score.toFixed(2) || 'NA', '')}
+                        {FOVMetadataItem('Dataset ID', fov?.pml_id || 'NA', '')}
+
+                        {/* show the src_filepath
+                        note that white-space: pre-line allows '\n' to create a newline */}
+                        <div className="" style={{whiteSpace: 'pre-line'}}>
+                            {fov?.src_filename.replace(/\//g, '\n') || 'NA'}
+                        </div>
+    
+                    </div>
+
+                    {/* FOV display */}
+                    <div className="w-66">
+                        <img width='66%' src={`${settings.apiUrl}/fovs/rgb/proj/${this.state.fovId}`}/>
+                    </div>
+
                 </div>
 
                 {/* thumbnail grid */}
                 <div className="w-100 thumbnail-grid">
-                    {this.data?.fovs.map(fov => thumbnail(fov, this.changeFov))}
+                    {this.data?.fovs.map(fov => thumbnail(fov, this.state.fovId, this.changeFov))}
                 </div>
 
 
@@ -107,3 +127,13 @@ export default class FOVCurator extends Component {
 }
 
 
+// warning: this is almost a direct copy of a function in header.jsx
+function FOVMetadataItem(label, value, units) {
+    return (
+        <div className='header-metadata-item pb2'>
+            <strong className='f4'>{value}</strong>
+            <abbr className='f5' title='units description'>{units}</abbr>
+            <div className='f6 header-metadata-item-label'>{label}</div>
+        </div>
+    );
+}

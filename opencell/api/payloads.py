6581ab8602a4669b6897e9bfdb0a863e8ac1791a
Keith Cheveralls <keith.chev@gmail.com>
2020-10-05 17:16:42 -0700

    add the ensg_id to the lines/ payload
    Former-commit-id: 3b1e8846a066275fa4ec3a0a52ad6a5e50cf4bac

--- opencell/api/payloads.py
+++ opencell/api/payloads.py
@@ -32,6 +32,7 @@ def generate_cell_line_payload(cell_line, included_fields, fov_count=None):
         'target_family': design.target_family,
         'target_terminus': design.target_terminus.value[0],
         'transcript_id': design.transcript_id,
+        'ensg_id': design.uniprot_metadata.ensg_id,
         'hek_tpm': design.hek_tpm,
     }
 
